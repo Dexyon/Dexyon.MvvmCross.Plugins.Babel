6d2fe90c4b8db82155db174536542e532f0f0c97
Erik van Seters <crusty@dexyon.com>
2014-12-08 20:49:54 +0100

    Added the files for the Core project

---
+++ Dexyon.MvvmCross.Plugins.Babel.Core/ViewModels/BaseBabelViewModel.cs.pp
@@ -0,0 +1,14 @@
+﻿using Cirrious.MvvmCross.Localization;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace $rootnamespace$.Core.ViewModels
+{
+    public abstract class BaseBabelViewModel
+        : MvxViewModel
+    {
+        public IMvxLanguageBinder TextSource
+        {
+            get { return new MvxLanguageBinder(Constants.GeneralNamespace, GetType().Name); }
+        }
+    }
+}	
